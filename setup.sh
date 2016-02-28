a292ef4c7aed15b742a2629b8a9f8311549e37ba
hekar <hekar@localhost.localdomain>
2016-02-28 17:12:36 -0500

    Updates to setup script. Added ubuntu

--- setup.sh
+++ setup.sh
@@ -1,19 +1,29 @@
 #!/bin/sh
 
+deb_packages="git git-gui vim g++ zsh docker docker-compose"
+deb_atom="https://github.com/atom/atom/releases/download/v1.5.3/atom-amd64.deb"
+deb_dropbox="https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.10.28_amd64.deb"
+
+rhel_packages="git vim gcc-c++ zsh git-gui docker docker-compose"
+rhel_atom="https://github.com/atom/atom/releases/download/v1.5.3/atom.x86_64.rpm"
+rhel_dropbox="https://www.dropbox.com/download?dl=packages/fedora/nautilus-dropbox-2015.10.28-1.fedora.x86_64.rpm"
+
 if python -mplatform | grep -q -i ubuntu; then
-  sudo apt-get update -y
-  sudo apt-get install -y git git-gui vim g++ zsh
+  sudo apt-get update -y && sudo apt-get install -y $deb_packages
+  wget $deb_atom
+  wget $deb_dropox
+  sudo dpkg -i $deb_atom
+  sudo dpkg -i $deb_packages
+  sudo apt-get install -f
 elif python -mplatform | grep -q -i fedora; then
   # Install RPM fusion
   sudo dnf install -y http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-  sudo dnf install -y git vim gcc-c++ zsh git-gui
-  sudo dnf install -y https://github.com/atom/atom/releases/download/v1.5.3/atom.x86_64.rpm
+  sudo dnf install -y $rhel_packages $rhel_atom $rhel_dropbox
 elif python -mplatform | grep -q -i centos; then
-  sudo dnf install -y git vim gcc-c++ zsh git-gui
-  sudo dnf install -y https://github.com/atom/atom/releases/download/v1.5.3/atom.x86_64.rpm
+  sudo dnf install -y $rhel_packages $rhel_atom $rhel_dropbox
   echo "CentOS is not fully supported"
 elif python -mplatform | grep -q -i opensuse; then
-  sudo zypper in -y git vim gcc-c++ zsh git-gui
+  sudo zypper in -y $rhel_packages
 else
   echo "Your Linux distribution is not supported"
 fi
