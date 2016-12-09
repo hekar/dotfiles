6b79ab26a39f7ddad7f7428ddff6e9f263426fa7
Hekar Khani <hekark@gmail.com>
2016-12-08 21:08:33 -0600

    Updated package versions. Replaced firefox with chrome

--- setup.sh
+++ setup.sh
@@ -3,14 +3,18 @@
 generic_elixir="https://github.com/elixir-lang/elixir.git"
 
 deb_packages="i3 git git-gui vim g++ zsh docker docker-compose htop"
-deb_atom="https://github.com/atom/atom/releases/download/v1.5.3/atom-amd64.deb"
+deb_atom="https://github.com/atom/atom/releases/download/v1.12.6/atom-amd64.deb"
 deb_dropbox="https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.10.28_amd64.deb"
 deb_erlang="https://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_19.0.2-1~ubuntu~xenial_amd64.deb"
+deb_chrome="https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm"
 
-rhel_packages="i3 git vim gcc-c++ zsh git-gui docker docker-compose wireshark htop ImageMagick-devel byzanz glew-devel"
-rhel_atom="https://atom-installer.github.com/v1.9.9/atom.x86_64.rpm?s=1471477229&ext=.rpm"
+rhel_packages="i3 git vim gcc-c++ zsh git-gui docker docker-compose wireshark htop ImageMagick-devel byzanz glew-devel cmake"
+rhel_atom="https://github.com/atom/atom/releases/download/v1.12.6/atom.x86_64.rpm"
 rhel_dropbox="https://www.dropbox.com/download?dl=packages/fedora/nautilus-dropbox-2015.10.28-1.fedora.x86_64.rpm"
 rhel_erlang="https://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_19.0~centos~7_amd64.rpm"
+rhel_chrome="https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm"
+rhel_jdk="http://download.oracle.com/otn-pub/java/jdk/8u111-b14/jdk-8u111-linux-x64.rpm"
+
 
 suse_packages="i3 git vim gcc-c++ zsh git-gui docker docker-compose wireshark htop"
 
@@ -23,22 +27,29 @@ if python -mplatform | grep -q -i ubuntu; then
   wget $deb_atom -O atom.deb
   wget $deb_dropbox -O dropbox.deb
   wget $deb_erlang -O erlang.deb
+  wget $deb_chrome -O chrome.deb
   sudo dpkg -i atom.deb
   sudo dpkg -i dropbox.deb
   sudo dpkg -i erlang.deb
+  sudo dpkg -i chrome.deb
   sudo apt install -f
-elif python -mplatform | grep -q -i fedora; then
+elif python3 -mplatform | grep -q -i fedora; then
   # Install RPM fusion
   dnf_install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
   dnf_install $rhel_packages
   dnf_install $rhel_atom
   dnf_install $rhel_dropbox
   dnf_install $rhel_erlang
+  dnf_install $rhel_chrome
+  dnf_install $rhel_jdk
+  sudo dnf remove -y java-1.8.0-openjdk-headless.x86_64
+  dnf_install libtool libtool-ltdl-devel libvterm-devel
 elif python -mplatform | grep -q -i opensuse; then
   zyp_in $suse_packages
   zyp_in $rhel_atom
   zyp_in $rhel_dropbox
   zyp_in $rhel_erlang
+  zyp_in $rhel_chrome
 else
   echo "Your Linux distribution is not supported"
 fi
@@ -47,11 +58,11 @@ fi
 # Install node version manager
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install 4 && nvm install 5 && nvm install 6
-nvm alias default 4
+nvm install 4 && nvm install 6
+nvm alias default 6
 
 # Install Atom Text Editor packages
-apm install editorconfig linter linter-eslint atom-beautify ensime
+apm install editorconfig linter linter-eslint atom-beautify
 
 # ============================
 # Create additional folders  in ~/
@@ -62,6 +73,7 @@ mkdir $HOME/github
 mkdir $HOME/share
 mkdir $HOME/tmp
 mkdir $HOME/.vim_backup
+rm -Rf $HOME/Documents $HOME/Music $HOME/Pictures $HOME/Public $HOME/Templates $HOME/Videos
 
 # ============================
 # Setup applications in ~/share
@@ -69,16 +81,11 @@ mkdir $HOME/.vim_backup
 pushd $HOME/share
 
 # Install Elixir
-git clone $generic_elixir && cd elixir && git checkout v1.2.3 && sudo make install
-
-# Firefox Developer Edition
-curl -o firefox.tar.bz2 "https://download-installer.cdn.mozilla.net/pub/firefox/nightly/latest-mozilla-aurora/firefox-47.0a2.en-US.linux-x86_64.tar.bz2"
-tar -jxvf firefox.tar.bz2
+git clone $generic_elixir && cd elixir && git checkout v1.3.4 && sudo make install
 
 # Neovim
 git clone https://github.com/neovim/neovim.git
 pushd neovim
-sudo dnf install -y libtool libtool-ltdl-devel libvterm-devel
 make
 sudo make install
 popd
