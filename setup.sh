61c0b5931c40831cbc7ef9498cf3c2bb1da9dab2
Hekar Khani <hekark@gmail.com>
2017-07-23 16:42:39 -0400

    Install GLIDE by default

--- setup.sh
+++ setup.sh
@@ -99,6 +99,7 @@ go get -u github.com/garyburd/redigo/redis
 go get -u github.com/Rafflecopter/golang-relyq/relyq
 go get -u github.com/Rafflecopter/golang-relyq/storage/redis
 go get -u github.com/bradfitz/gomemcache/memcache
+curl https://glide.sh/get | sh
 
 # Install dotfiles
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &&
