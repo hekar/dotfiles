5709cca668fe95685b0b706030f841538231ba9c
Hekar Khani <hekark@gmail.com>
2017-12-07 20:30:09 -0500

    Fixed multiple issues in setup script

--- setup.sh
+++ setup.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-chrome_package="https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm"
-
 suse_packages="git vim gcc-c++ zsh git-gui docker wireshark htop cmake stow make go go-doc libgit2-devel automake i3 i3lock i3status tmux rxvt-unicode urxvt-font-size libtool emacs xclip"
+chrome_package="https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm"
 
 shopt -s expand_aliases
 alias zyp_in='sudo zypper install -y'
-echo -e "\e[33m Assuming you are running OpenSUSE or OpenSUSE Tumbleweed"
-wget https://dl.google.com/linux/linux_signing_key.pub
-sudo rpm --import linux_signing_key.pub
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/zypp/repos.d/vscode.repo'
+
 sudo zypper refresh
-sudo zypper install -y code
 zyp_in $suse_packages
 zyp_in $chrome_package
 
@@ -20,10 +14,10 @@ zyp_in $chrome_package
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 
 # Install node version manager
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install 6 && nvm install 8
-nvm alias default 6
+nvm install 8
+nvm alias default 8
 
 # ============================
 # Create additional folders  in ~/
@@ -49,6 +43,7 @@ pushd neovim
 make
 sudo make install
 sudo pip3 install --upgrade pip && pip3 install --user neovim
+npm install -g neovim
 popd
 
 # youtube-open
@@ -64,10 +59,10 @@ git clone git@github.com:hekar/gitmark.git
 popd # $HOME/github/go/src/github.com/hekar
 
 # Install codecs
-zypper addrepo -f http://packman.inode.at/suse/openSUSE_Tumbleweed/ packman
-zypper addrepo -f http://packman.inode.at/suse/openSUSE_Tumbleweed/ packman
-zypper install ffmpeg lame gstreamer-plugins-bad gstreamer-plugins-ugly gstreamer-plugins-ugly-orig-addon gstreamer-plugins-libav libdvdcss2
-zypper dup --from http://packman.inode.at/suse/openSUSE_Tumbleweed/
+sudo zypper addrepo -f http://packman.inode.at/suse/openSUSE_Tumbleweed/ packman
+sudo zypper addrepo -f http://packman.inode.at/suse/openSUSE_Tumbleweed/ packman
+sudo zypper install -y ffmpeg lame gstreamer-plugins-bad gstreamer-plugins-ugly gstreamer-plugins-ugly-orig-addon gstreamer-plugins-libav libdvdcss2
+sudo zypper dup --from http://packman.inode.at/suse/openSUSE_Tumbleweed/
 
 # Golang dependencies
 go get -u github.com/constabulary/gb/...
@@ -86,7 +81,7 @@ curl https://glide.sh/get | sh
 # Install dotfiles
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &&
 git clone git@github.com:hekar/dotfiles.git &&
-rm ~/.bashrc ~/.config/user-dirs.dirs ~/.config/user-dirs.locale ~/.profile &&
+rm ~/.bashrc ~/.config/user-dirs.dirs ~/.config/user-dirs.locale ~/.profile ~/.gitconfig ~/.zshrc &&
 cd ./dotfiles &&
 stow .
 
