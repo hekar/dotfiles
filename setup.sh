5a1338337effa0bc974f9efe75ec0f4f09a9a1d2
Hekar Khani <hekark@gmail.com>
2023-02-14 21:35:24 -0500

    chore: updated packages list
    - fix: bug with installin packages list
    - chore: small formatting changes to setup.sh
    - chore: rename packages.txt to default_packages

--- setup.sh
+++ setup.sh
@@ -5,20 +5,24 @@ set -xe
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Install default packages
-packages=(readarray -t a < $SCRIPT_DIR/packages.txt)
+readarray -t packages < $SCRIPT_DIR/.default_packages
 
-sudo pacman -Syu $packages || true
+sudo pacman -Syu "${packages[@]}"
+
+set +e
 
 # Install https://github.com/asdf-vm/asdf
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2 || true
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
+
+set -e
 
 # Install volta version manager
 curl https://get.volta.sh | bash
 
-$HOME/.volta/bin/volta install node@18
+$HOME/.volta/bin/volta install "node@18"
 
 # ============================
-# Create additional folders  in ~/
+# Create additional folders in ~
 
 mkdir -p $HOME/bin \
     $HOME/c \
@@ -28,14 +32,22 @@ mkdir -p $HOME/bin \
 # Install OhMyZsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" || true
 
+set +e
+
 # Install dotfiles
-git clone git@github.com:hekar/dotfiles.git &&
-rm ~/.bashrc ~/.profile ~/.gitconfig ~/.zshrc &&
-cd ./dotfiles &&
-stow . || true
+git clone git@github.com:hekar/dotfiles.git
+
+rm ~/.bashrc
+rm ~/.profile
+rm ~/.gitconfig
+rm ~/.zshrc
+cd ./dotfiles
+stow .
 
 # Install tmuxp
 pip install --user tmuxp || true
+
+set -e
 
 # Install asdf plugins
 asdf_install () {
